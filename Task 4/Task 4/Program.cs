6e41a1fcb0043d9beed93d29f2dafe1c293f7c54
BoguslawP1 <boguslaw.p1@outlook.com>
2023-09-22 20:23:07 +0200

    Pierwszy Lekcja

---
+++ Task 4/Task 4/Program.cs
@@ -0,0 +1,20 @@
+﻿string name = ("Ewa");
+string sex = ("Woman");
+var age = 33;
+
+if (age == 33 && sex == "Woman" && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (age < 30 && sex == "Woman")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (age < 18 && sex != "Woman")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
+else
+{
+    Console.WriteLine("Niezidentyfikowana osoba.");
+}
